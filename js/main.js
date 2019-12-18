f6f840c348a34e5c86453fa9ae7d2e13b7953121
lenocheklisichka <47819860+lenocheklisichka@users.noreply.github.com>
2019-12-18 19:00:41 +0300

    бургер меню при адаптиве

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 
-$('.menu-btn').on('click', function(e) {
-  event().preventDefault();
-  $(this).toggleClass('menu-btn_active');
-  $('.menu-nav').toggleClass('menu-nav_active');
+$(document).ready(function(){
+$(".menu-btn").on("click", function(e) {
+  e.preventDefault();
+  $(this).toggleClass("menu-btn_active");
+  $(".menu-nav").toggleClass("menu-nav_active");
 });
-
+});
 
